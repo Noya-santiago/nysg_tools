b460d8eef8d2f3ba21e7a4cb03f3309a3ad2196f
Santiago Noya <noyasantiagomail@gmail.com>
2024-08-14 13:10:29 -0300

    Deleted some files and added more funcoitns

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="nysg_tools",
-    version="0.2",
+    version="0.4",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
     description="Una colección de herramientas utiles para los laboratorios de la UBA FCEN",
